04d40091a03a52f3312697ab257878555e2dc857
13516109 <13516109@std.stei.itb.ac.id>
2019-03-11 23:20:39 +0700

    Added utility files
    * Added letter rendering
    * Changed color fill using RGB struct, instead of individual red green blue char

---
+++ util/func_util.h
@@ -0,0 +1,23 @@
+#ifndef __FUNC_UTIL_HPP__
+#define __FUNC_UTIL_HPP__
+
+#include <linux/fb.h>
+
+using namespace std;
+
+struct RGB {
+    char r;
+    char g;
+    char b;
+};
+
+void clear_screen(unsigned int x_size, unsigned int y_size, char * framebuffer, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+
+void bresenham(int x0, int y0, int x1, int y1, struct RGB rgb, char colorful, char * framebuffer, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+
+void pixelColor(struct RGB rgb, char *fbp, long int location);
+
+char checkPixelAround(int x, int y, char * fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void rasterScan(int x_min, int y_min, int x_max, int y_max, struct RGB rgb, char * fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+
+#endif
---
+++ util/name.h
@@ -0,0 +1,31 @@
+#ifndef __NAME_HPP__
+#define __NAME_HPP__
+#include <linux/fb.h>
+#include "func_util.h"
+using namespace std;
+
+void render_a(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_b(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_c(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_d(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_e(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_f(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_g(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_h(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_i(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_j(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_k(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_l(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_m(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_n(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_o(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_p(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_r(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_s(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_t(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_u(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_v(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_w(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+void render_y(int x, int y, struct RGB rgb, char *fbp, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo);
+
+#endif
