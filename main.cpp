7d9ab7e0078ef5bed6f72556fd75a16ed6f91550
13516109 <13516109@std.steit.itb.ac.id>
2019-03-14 08:58:22 +0700

    Added window

--- main.cpp
+++ main.cpp
@@ -3,17 +3,84 @@
 #include <sys/mman.h>
 #include <sys/ioctl.h>
 #include <linux/fb.h>
+#include <linux/input.h>
 #include <iostream>
 #include <unistd.h>
 #include <ncurses.h>
+#include <cstdlib>
+#include <cstring>
+#include <thread>
 
 #include "util/func_util.h"
 #include "objects.h"
 
 using namespace std;
 
-#define clear() printf("\033[H\033[J")
-#define gotoxy(x,y) printf("\033[%d;%dH", (x), (y))
+char menu_bar_option_selection = 0;
+char menu_bar_selection = 0;
+
+void userInput(int fd){
+    ssize_t n;
+    struct input_event ev;
+
+    bool out = false;
+
+    while(1){
+        n = read(fd, &ev, sizeof ev);
+        if (n == (ssize_t)-1){
+            if (errno == EINTR){
+                continue;
+            } else {
+                break;
+            }
+        } else if (n != sizeof ev){
+            errno = EIO;
+            break;
+        }
+
+        if (ev.type == EV_KEY && ev.value == 1){
+            switch (ev.code){
+                
+            }
+        }
+    }
+}
+
+void drawMenuBar(WINDOW** menu_bar_window){
+    char ** menu_bar_options;
+
+    switch(menu_bar_selection){
+        case(0):
+            menu_bar_options = (char**) malloc (5 * sizeof (char *));
+            menu_bar_options[0] = "File";
+            menu_bar_options[1] = "New";
+            menu_bar_options[2] = "Open";
+            menu_bar_options[3] = "Save";
+            menu_bar_options[4] = "Exit";
+            break;
+        case(1):
+            menu_bar_options = (char**) malloc (5 * sizeof (char *));
+            menu_bar_options[0] = "File";
+            menu_bar_options[1] = "New";
+            menu_bar_options[2] = "Open";
+            menu_bar_options[3] = "Save";
+            menu_bar_options[4] = "Exit";
+            break;
+        case(2):
+
+        case(3):
+
+        default:
+            break;
+    }
+
+    box(menu_bar_window[0],0,0);
+    mvwprintw(menu_bar_window[0],0,0,menu_bar_options[0]);
+    mvwprintw(menu_bar_window[0],1,0,menu_bar_options[1]);
+    mvwprintw(menu_bar_window[0],2,0,menu_bar_options[2]);
+    mvwprintw(menu_bar_window[0],3,0,menu_bar_options[3]);
+    mvwprintw(menu_bar_window[0],4,0,menu_bar_options[4]);
+}
 
 int main(int argc, char** argv){
     int fbfd;
@@ -48,14 +115,45 @@ int main(int argc, char** argv){
         exit(4);
     }
 
-    struct RGB rgb;
-    rgb.r = 255;
-    rgb.g = 0;
-    rgb.b = 0;
+    const char * dev = "/dev/input/event2";
+    int fd = open(dev,O_RDONLY);
+    if (fd == -1){
+        fprintf(stderr, "Cannot open %s: %s.\n", dev, strerror(errno));
+        return EXIT_FAILURE;
+    }
 
-    Line line(new Point(100,100), new Point(100,100));
+    // thread inputter (userInput,fd);
 
-    line.render(fbp,vinfo,finfo);
+    struct RGB menu_bar_background;
+    menu_bar_background.r = 204;
+    menu_bar_background.g = 204;
+    menu_bar_background.b = 204;
+
+    initscr();
+
+    WINDOW ** menu_bar_window = (WINDOW**) malloc (4*sizeof(WINDOW*));
+    menu_bar_window[0] = newwin(6,15,0,0);
+    drawMenuBar(menu_bar_window);
+    
+    // move(0,0);
+    // printw("File");
+    // move(0,8);
+    // printw("View");
+    // move(0,16);
+    // printw("Attribute");
+    // move(0,29);
+    // printw("Object");
+    
+    refresh();
+    wrefresh(menu_bar_window[0]);
+
+    delwin(menu_bar_window[0]);
+    free(menu_bar_window);
+    endwin();
+
+    // Line line(new Point(100,100), new Point(100,100));
+
+    // line.render(fbp,vinfo,finfo);
 
     // Polygon polygon;
 
